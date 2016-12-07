f4132d95994e644d075617ef3337ee6f3e0a3196
Aivis Abele <aabele@gmail.com>
2016-12-08 00:29:36 +0200

    Fixed package name in setup.py.

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import os
 from setuptools import find_packages, setup
 
-# with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-#     README = readme.read()
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='ss-scraper',
+    name='ssscraper',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
     description='A simple python selenium wrapper for ss.lv website',
-    # long_description=README,
-    url='https://www.example.com/',
+    long_description=README,
+    url='https://www.ss.lv/',
     author='Aivis Abele',
     author_email='aabele@gmail.com',
     classifiers=[
