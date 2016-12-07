a13441c252160109b03d6cae4ece34b5fcac0ae1
Aivis Abele <aabele@gmail.com>
2016-12-08 00:27:52 +0200

    * Added readme * Added setup.py

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python Selenium scraper for ss.lv website
+
+## Quickstart
+* `npm install phantomjs`
+* `pip install git+ssh://git@github.com/aabele/ssscraper.git#egg=ssscraper`
+
+Use at your own risk!
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+# with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+#     README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='ss-scraper',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='A simple python selenium wrapper for ss.lv website',
+    # long_description=README,
+    url='https://www.example.com/',
+    author='Aivis Abele',
+    author_email='aabele@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
